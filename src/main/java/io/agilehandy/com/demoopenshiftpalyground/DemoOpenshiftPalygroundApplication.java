be389618d5442a4a09204a15cfa2f02219f38709
Haytham Mohamed <haybu@hotmail.com>
2021-08-16 14:22:22 -0500

    init

---
+++ src/main/java/io/agilehandy/com/demoopenshiftpalyground/DemoOpenshiftPalygroundApplication.java
@@ -0,0 +1,22 @@
+package io.agilehandy.com.demoopenshiftpalyground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+public class DemoOpenshiftPalygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOpenshiftPalygroundApplication.class, args);
+	}
+
+	class MyRestController {
+
+		@GetMapping
+		public Mono<String> ping() {
+			return Mono.just("Hello from OpenShift!");
+		}
+	}
+}
---
+++ src/test/java/io/agilehandy/com/demoopenshiftpalyground/DemoOpenshiftPalygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package io.agilehandy.com.demoopenshiftpalyground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOpenshiftPalygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
